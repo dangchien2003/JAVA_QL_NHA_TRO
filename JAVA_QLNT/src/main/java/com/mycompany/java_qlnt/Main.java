934a6623be9eb45a26f2eabc03a22ae770962771
dangchien2003 <chienboy03@gmail.com>
2023-09-26 22:52:01 +0700

    update new project
    QLBH to QLNT

---
+++ JAVA_QLNT/src/main/java/com/mycompany/java_qlnt/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_qlnt;
+import com.mycompany.java_qlnt.helper.Helper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author chien
+ */
+public class Main {
+    public static void main(String[] args) {
+        Helper helper = new Helper();
+//        Object rs = helper.query("insert into test(test) values (2)");
+        Object rs = helper.query("select * from test");
+        if( rs== null){
+            System.out.print("null"); 
+        }else {
+            try {
+                ResultSet result = (ResultSet)rs;
+                while(result.next()) {
+                    try {
+                        int cotSoNguyen = result.getInt("test");
+                        System.out.println(cotSoNguyen); 
+                    } catch (SQLException ex) {
+                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
